0cbd863190b0b4f4ff6b328cd42c786304ef0ad4
Joe Wroe <joe_wroe@icloud.com>
2018-12-08 11:41:00 +0000

    Clean up React App boilerplate and add initial state object
    Joe

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+  state = {
+    todos: [
+        { id: 1, content: 'Buy some Milk' },
+        { id: 2, content: 'Play some Mario Kart' }
+    ]
+  }
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
       </div>
     );
   }
